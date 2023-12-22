b8fe361aca87701bfb27b6807d2e55df17888eef
my-selmis <132098592+my-selmis@users.noreply.github.com>
2023-12-22 15:15:22 +0300

    Стрелы + Элементали + Передвижение + Здоровье

---
+++ Assets/Scripts/Стрела.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Стрела : MonoBehaviour
+{
+    [SerializeField] private float SpellSpeed;
+    private Rigidbody2D spellBody;
+    private float spellXPosition;
+
+    // Start is called before the first frame update
+    void Awake(){
+        spellBody = GetComponent<Rigidbody2D>();
+
+    }
+    void Start()
+    {
+        spellBody.velocity = new Vector2(SpellSpeed,0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        spellXPosition = transform.position.x;
+        // print(spellXPosition);
+        if ( spellXPosition > 11 ){
+            Destroy(gameObject);
+            // print("Entered if");
+        }
+    }
+}
