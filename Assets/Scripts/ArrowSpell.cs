675d0d6fef091515428ef873385b89700e3f652c
my-selmis <132098592+my-selmis@users.noreply.github.com>
2023-12-26 14:45:12 +0300

    Реализовал окно произнесения с функционалом

--- Assets/Scripts/ArrowSpell.cs
+++ Assets/Scripts/ArrowSpell.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Элементаль : MonoBehaviour
+public class ArrowSpell : MonoBehaviour
 {
     [SerializeField] private float SpellSpeed;
-    [SerializeField] private float ExplosionTime;
-    private Transform Explosion;
     private Rigidbody2D spellBody;
     private float spellXPosition;
 
     // Start is called before the first frame update
     void Awake(){
         spellBody = GetComponent<Rigidbody2D>();
-        Explosion = this.gameObject.transform.GetChild(0);
+
     }
     void Start()
     {
         spellBody.velocity = new Vector2(SpellSpeed,0);
-        
     }
 
     // Update is called once per frame
@@ -26,16 +24,9 @@ public class Элементаль : MonoBehaviour
     {
         spellXPosition = transform.position.x;
         // print(spellXPosition);
-        if ( spellXPosition >= 4.5f ){
-            StartCoroutine(ExplosionActive());
+        if ( spellXPosition > 11 ){
+            Destroy(gameObject);
+            // print("Entered if");
         }
-    }
-
-    IEnumerator ExplosionActive(){
-        // print("entered");
-        spellBody.velocity = new Vector2(0,0);
-        Explosion.gameObject.SetActive(true);
-        yield return new WaitForSeconds(ExplosionTime);
-        Destroy(gameObject);
     }
 }
--- Assets/Scripts/ElementalSpell.cs
+++ Assets/Scripts/ElementalSpell.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class Стрела : MonoBehaviour
+public class ElementalSpell : MonoBehaviour
 {
     [SerializeField] private float SpellSpeed;
+    [SerializeField] private float ExplosionTime;
+    private Transform Explosion;
     private Rigidbody2D spellBody;
     private float spellXPosition;
 
     // Start is called before the first frame update
     void Awake(){
         spellBody = GetComponent<Rigidbody2D>();
-
+        Explosion = this.gameObject.transform.GetChild(0);
     }
     void Start()
     {
         spellBody.velocity = new Vector2(SpellSpeed,0);
+        
     }
 
     // Update is called once per frame
@@ -24,9 +26,16 @@ public class Стрела : MonoBehaviour
     {
         spellXPosition = transform.position.x;
         // print(spellXPosition);
-        if ( spellXPosition > 11 ){
-            Destroy(gameObject);
-            // print("Entered if");
+        if ( spellXPosition >= 4.5f ){
+            StartCoroutine(ExplosionActive());
         }
+    }
+
+    IEnumerator ExplosionActive(){
+        // print("entered");
+        spellBody.velocity = new Vector2(0,0);
+        Explosion.gameObject.SetActive(true);
+        yield return new WaitForSeconds(ExplosionTime);
+        Destroy(gameObject);
     }
 }
